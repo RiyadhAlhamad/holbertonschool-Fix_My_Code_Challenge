a5115da48b12a71ea55486b5dc280fc920d5155f
Your Name <you@example.com>
2025-09-25 08:38:57 +0000

    edit

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,36 +1,28 @@
 #!/usr/bin/python3
-""" FizzBuzz """
 import sys
 
-
-def fizzbuzz(n):
+def fizzbuzz(n: int) -> None:
     if n < 1:
         return
+    print(" ".join(
+        ("FizzBuzz" if i % 15 == 0 else
+         "Fizz" if i % 3 == 0 else
+         "Buzz" if i % 5 == 0 else
+         str(i))
+        for i in range(1, n + 1)
+    ))
 
-    out = []
-    for i in range(1, n + 1):
-        if i % 15 == 0:              # 3*5
-            out.append("FizzBuzz")
-        elif i % 3 == 0:
-            out.append("Fizz")
-        elif i % 5 == 0:
-            out.append("Buzz")
-        else:
-            out.append(str(i))
-    print(" ".join(out))
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(sys.argv) <= 1:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
-
     try:
         number = int(sys.argv[1])
     except ValueError:
         print("Missing number")
         print("Usage: ./0-fizzbuzz.py <number>")
         print("Example: ./0-fizzbuzz.py 89")
-        sys.exi
+        sys.exit(1)
+    fizzbuzz(number)
