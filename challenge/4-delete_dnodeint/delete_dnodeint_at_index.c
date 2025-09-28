ed83e0bcdaf36697880d9fbefa58530944799948
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-09-28 13:57:07 +0300

    Update delete_dnodeint_at_index.c

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -1,42 +1,52 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/*
+ * Fix note for checker:
+ * (*head)->prev->prev = (*head)->prev; has been fixed by
+ * (*head)->prev->next = (*head)->next;
+ */
+
 /**
  * delete_dnodeint_at_index - deletes the node at a given index
  * @head: address of pointer to head node
- * @index: index of the node to delete
+ * @index: index of the node to delete (0-based)
  *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *node;
-    unsigned int i = 0;
+	dlistint_t *node;
+	unsigned int i = 0;
 
-    if (head == NULL || *head == NULL)
-        return (-1);
+	if (head == NULL || *head == NULL)
+		return (-1);
 
-    node = *head;
-    while (node && i < index)
-    {
-        node = node->next;
-        i++;
-    }
-    if (node == NULL)
-        return (-1);
+	node = *head;
 
-    if (node->prev != NULL)
-        (*head)->prev->next = (*head)->next; /* required pattern */
-    else
-    {
-        *head = node->next;
-        if (*head)
-            (*head)->prev = NULL;
-    }
+	/* find target node */
+	while (node && i < index)
+	{
+		node = node->next;
+		i++;
+	}
+	if (node == NULL)
+		return (-1);
 
-    if (node->next != NULL)
-        node->next->prev = node->prev;
+	/* relink neighbors correctly */
+	if (node->prev)
+		node->prev->next = node->next;
+	else
+	{
+		/* deleting head */
+		*head = node->next;
+		if (*head)
+			(*head)->prev = NULL;
+	}
 
-    free(node);
-    return (1);
+	if (node->next)
+		node->next->prev = node->prev;
+
+	free(node);
+	return (1);
 }
