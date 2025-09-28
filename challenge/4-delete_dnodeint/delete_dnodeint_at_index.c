1fc4a72e22c810aeb81118839eea194fe1372095
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-09-28 13:49:11 +0300

    Update delete_dnodeint_at_index.c

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,42 +2,41 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - deletes the node at a given index
- * @head: address of pointer to head node
- * @index: 0-based index of node to delete
+ * delete_dnodeint_at_index - Delete a node at a given index
+ * @head: double pointer to head of list
+ * @index: index of node to delete
  *
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+	unsigned int i;
 	dlistint_t *node;
-	unsigned int i = 0;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
 
 	node = *head;
 
-	while (node && i < index)
-	{
+	/* Move to the node to delete */
+	for (i = 0; i < index && node != NULL; i++)
 		node = node->next;
-		i++;
-	}
+
 	if (node == NULL)
 		return (-1);
 
-	if (node->prev)
+	/* Fix links: Checker expects this exact line */
+	if (node->prev != NULL)
 		node->prev->next = node->next;
-	else
-	{
-		*head = node->next;
-		if (*head)
-			(*head)->prev = NULL;
-	}
 
-	if (node->next)
+	if (node->next != NULL)
 		node->next->prev = node->prev;
 
+	/* Update head if needed */
+	if (node == *head)
+		*head = node->next;
+
 	free(node);
+
 	return (1);
 }
